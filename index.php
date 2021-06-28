cb483f497754f62ce2432970d0415ea8b6ff7e6b
Paul Garcia <paulgr30@hotmail.com>
2021-06-28 15:54:47 -0500

    agregamos el index

---
+++ index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="description" content="coca cola la bebida" />
+    <meta name="keywords" content="coca, coca cola, bebida" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Inicio Coca cola</title>
+
+    <link rel="shortcut icon" href="img/favicon.png" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;1,400&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="style.css" />
+
+    <script src="https://kit.fontawesome.com/a335805ab4.js" crossorigin="anonymous"></script>
+  </head>
+
+  <body>
+    <header>
+      <div class="container">
+        <a href="">
+          <img src="img/logo.svg" class="logo" />
+        </a>
+        <nav>
+          <a href="#inicio">Inicio</a>
+          <a href="#nosotros">Nosotros</a>
+          <a href="#servicios">Servicios</a>
+          <a href="#galeria">Galeria</a>
+          <a href="#contactenos">Contáctenos</a>
+        </nav>
+        <a href="#" class="hamb"><i class="fas fa-bars"></i></a>
+      </div>
+    </header>
+    <main>
+      <section id="inicio">
+        <img src="img/bannerprincipal.jpg" alt="" />
+        <div class="bloque-inicio">
+          <h1>Bienvenidos a Coca Cola</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+          <a href="#nosotros" class="boton boton-rojo">Ver más</a>
+        </div>
+      </section>
+      <section id="nosotros" class="seccion">
+        <div class="container">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+            temporibus eligendi in voluptate harum. Distinctio, mollitia numquam
+            autem ullam sint perspiciatis possimus, quae quos, aut amet laborum
+            doloremque dolore accusamus.
+          </p>
+        </div>
+      </section>
+      <section id="servicios" class="seccion">
+        <div class="container">
+          <div class="row">
+            <div class="columna columna-33 columna-mobile-100">
+              <div class="bloque-servicio">
+                <div class="bloque-img-servicio cuadrado-perfecto">
+                  <img src="img/servicio1.jpg" />
+                </div>
+                <div class="bloque-contenido-servicio">
+                  <h3>Servicio 1</h3>
+                  <p>
+                    minima sed nam nihil, saepe cum tempore ipsum illum, quis
+                    assumenda fugit cumque iste reprehenderit libero corrupti
+                    hic. nisi amet cupiditate quas voluptate, hic architecto nam
+                    ex ab velit. Minima vero dolor cumque id officiis dolorem
+                    unde dignissimos.
+                  </p>
+                  <a href="#" class="boton boton-blanco">Ver más</a>
+                </div>
+              </div>
+            </div>
+            <div class="columna columna-33 columna-mobile-100">
+              <div class="bloque-servicio">
+                <div class="bloque-img-servicio cuadrado-perfecto">
+                  <img src="img/servicio2.jpg" />
+                </div>
+                <div class="bloque-contenido-servicio">
+                  <h3>Servicio 2</h3>
+                  <p>
+                    minima sed nam nihil, saepe cum tempore ipsum illum, quis
+                    assumenda fugit cumque iste reprehenderit libero corrupti
+                    hic. nisi amet cupiditate quas voluptate, hic architecto nam
+                    ex ab velit. Minima vero dolor cumque id officiis dolorem
+                    unde dignissimos.
+                  </p>
+                  <a href="#" class="boton boton-blanco">Ver más</a>
+                </div>
+              </div>
+            </div>
+            <div class="columna columna-33 columna-mobile-100">
+              <div class="bloque-servicio">
+                <div class="bloque-img-servicio cuadrado-perfecto">
+                  <img src="img/servicio3.jpg" />
+                </div>
+                <div class="bloque-contenido-servicio">
+                  <h3>Servicio 3</h3>
+                  <p>
+                    minima sed nam nihil, saepe cum tempore ipsum illum, quis
+                    assumenda fugit cumque iste reprehenderit libero corrupti
+                    hic. nisi amet cupiditate quas voluptate, hic architecto nam
+                    ex ab velit. Minima vero dolor cumque id officiis dolorem
+                    unde dignissimos.
+                  </p>
+                  <a href="#" class="boton boton-blanco">Ver más</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="galeria">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="columna-33 columna-mobile-50">
+              <div class="cuadrado-perfecto">
+                <img src="img/servicio1.jpg" />
+                <h4>Imagen 1</h4>
+              </div>
+            </div>
+            <div class="columna-33 columna-mobile-50">
+              <div class="cuadrado-perfecto">
+                <img src="img/servicio2.jpg" />
+                <h4>Imagen 2</h4>
+              </div>
+            </div>
+            <div class="columna-33 columna-mobile-50">
+              <div class="cuadrado-perfecto">
+                <img src="img/servicio3.jpg" />
+                <h4>Imagen 3</h4>
+              </div>
+            </div>
+            <div class="columna-33 columna-mobile-50">
+              <div class="cuadrado-perfecto">
+                <img src="img/servicio1.jpg" />
+                <h4>Imagen 4</h4>
+              </div>
+            </div>
+            <div class="columna-33 columna-mobile-50">
+              <div class="cuadrado-perfecto">
+                <img src="img/servicio2.jpg" />
+                <h4>Imagen 5</h4>
+              </div>
+            </div>
+            <div class="columna-33 columna-mobile-50">
+              <div class="cuadrado-perfecto">
+                <img src="img/servicio3.jpg" />
+                <h4>Imagen 6</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="contactenos" class="seccion">
+        <iframe
+          width="520"
+          height="400"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+          id="gmap_canvas"
+          src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Piura+(peru)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        ></iframe>
+
+        <div class="container-fluid">
+          <div class="row">
+            <div
+              class="
+                columna columna-41
+                empujar-58
+                columna-mobile-100
+                empujar-mobile-0
+                sinpadding-mobile
+              "
+            >
+              <form action="index.php" method="POST">
+                <div class="form-block">
+                  <input
+                    type="text"
+                    name="nombre"
+                    class="form-control"
+                    placeholder="nombre"
+                    require
+                  />
+                </div>
+                <div class="form-block">
+                  <input
+                    type="email"
+                    name="email"
+                    class="form-control"
+                    placeholder="email"
+                    require
+                  />
+                </div>
+                <div class="form-block">
+                  <textarea name="mensaje" placeholder="mensaje" require></textarea>
+                </div>
+                <div class="form-block bloque-ultimo">
+                  <input
+                    type="submit"
+                    class="boton boton-negro"
+                    value="enviar"
+                  />
+                </div>
+                <?php
+                  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $nombre = $_POST['nombre'];
+                    $email = $_POST['email'];
+                    $mensaje = $_POST['mensaje'];
+
+                    $para = "parami@hotmail.com";
+                    $asunto = "test de prueba";
+                    $cuerpo = $nombre."\n".$email."\n".$mensaje;
+                    $adicional = "From: noreply@gmail.com";
+
+                    mail($para, $asunto, $cuerpo, $adicional);
+                ?>
+                  <p>Envio exitoso</p>
+                <?php
+                  }
+                ?>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="columna columna-25 columna-mobile-100">
+            <img src="img/logo-blanco.png" class="logo-footer" />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Architecto asperiores iste animi commodi, accusamus harum, impedit
+              vitae fugit ducimus tenetur aliquam sunt voluptatum. Magni
+              voluptatem eligendi laboriosam sed, suscipit dolor.
+            </p>
+          </div>
+          <div class="columna columna-25  columna-mobile-100">
+            <h3>Temas relacionados</h3>
+            <ul>
+              <li><a href="">Tema 1</a></li>
+              <li><a href="">Tema 2</a></li>
+              <li><a href="">Tema 3</a></li>
+            </ul>
+          </div>
+          <div class="columna columna-25  columna-mobile-100">
+            <h3>Datos de contacto</h3>
+            <ul>
+              <li>empresa@hotmail.com</li>
+              <li><a href="">+51 9525203669</a></li>
+              <li>
+                Av. Universitario 2349 Pueblo Libre<br />
+                Lima - Peru
+              </li>
+            </ul>
+          </div>
+          <div class="columna columna-25 columna-mobile-100">
+            <h3>Redes sociales</h3>
+            <ul class="redes">
+              <li>
+                <a href="">
+                  <i class="fab fa-facebook-square"></i>
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <i class="fab fa-twitter-square"></i>
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <i class="fab fa-instagram-square"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="barra-footer">&copy; Derechos Reservados</div>
+    </footer>
+
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/funciones.js"></script>
+  </body>
+</html>
